e642f2240fbf24ba0156c26e5cfef5b0e05df689
David Grabosky <david.grabosky0401@gmail.com>
2022-04-28 10:11:03 -0700

    Added extra logging and FTM creation

--- plaidFtm.py
+++ plaidFtm.py
@@ -28,12 +28,14 @@ confirmation = input(
     'Have you completed the Plaid Quickstart workflow (Y/N)?: ').lower()
 
 if confirmation != 'y':
-    logging.warn(
+    logging.warning(
         'Please complete the quickstart workflow at: https://plaid.com/docs/quickstart/ and then re-run this script.')
 else:
     accessToken = input('ENTER ACCESS TOKEN: ')
 
     # Retrieve Plaid account id via Plaid access token
+
+    logging.info('Retrieving Plaid account...')
 
     path = f'{PLAID_ENV}/accounts/get'
     headers = {'Content-Type': 'application/json'}
@@ -44,9 +46,11 @@ else:
     })
     r = requests.post(path, headers=headers, data=payload).json()
     account_id = r['accounts'][0]['account_id']
-    logging.info(account_id)
+    logging.info('Plaid Account ID: ' + account_id)
 
     # Create processor token with Plaid account ID and Plaid access token
+
+    logging.info('Creating Plaid processor token...')
 
     path = f'{PLAID_ENV}/processor/token/create'
     headers = {'Content-Type': 'application/json'}
@@ -59,3 +63,21 @@ else:
     })
     r = requests.post(path, headers=headers, data=payload).json()
     logging.info(json.dumps(r, indent=2, sort_keys=True))
+    processorToken = r['processor_token']
+
+    # Create funds transfer method with Plaid processor token
+
+    path = f'{PT_ENV}/v2/funds-transfer-methods'
+    payload = json.dumps({
+        "data": {
+            "type": "funds-transfer-method",
+            "attributes": {
+                "funds-transfer-type": "ach",
+                "ach-check-type": "personal",
+                "contact-id": os.environ.get('CONTACT_ID'),
+                "plaid-processor-token": processorToken
+            }
+        }
+    })
+    r = requests.post(path, headers=HEADERS, data=payload).json()
+    logging.info('Funds transfer method created with an ID of: ' + r['data']['id'])
